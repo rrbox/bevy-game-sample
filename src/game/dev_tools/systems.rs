96728280713abb7c6c67949589a13cdadc354e9a
rrbox <87851278+rrbox@users.noreply.github.com>
2025-08-31 22:19:45 +0900

    Fix formatting in force_ingame_state_system function parameters

--- src/game/dev_tools/systems.rs
+++ src/game/dev_tools/systems.rs
@@ -4,7 +4,7 @@ use bevy::prelude::*;
 // 起動時にAppStateをInGameに設定するシステム
 pub fn force_ingame_state_system(
     mut next_app_state: ResMut<NextState<AppState>>,
-    mut next_game_state: ResMut<NextState<GameState>>
+    mut next_game_state: ResMut<NextState<GameState>>,
 ) {
     next_app_state.set(AppState::InGame);
     next_game_state.set(GameState::Moving);
